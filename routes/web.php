df18c756b0b0c861cb7d936a0f8c1d21b3caeefb
mrhanafi <muhdhanafirahman@gmail.com>
2025-06-19 14:53:08 +0800

    update livewire

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use App\Livewire\Game;
 use Illuminate\Support\Facades\Route;
+use Livewire\Livewire;
 
 Route::get('/', Game::class)->name('home');
+Livewire::setScriptRoute(function ($handle) { return Route::get('/livewire/livewirejs', $handle); });
